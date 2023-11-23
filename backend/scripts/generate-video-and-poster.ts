f5a6770a0c35a98f1bbd87e137bab97e6082b34a
Johan Oliwsson <johan@oliwsson.com>
2023-11-23 21:23:14 +0100

    Improvements

--- backend/scripts/generate-video-and-poster.ts
+++ backend/scripts/generate-video-and-poster.ts
@@ -9,48 +9,39 @@ function scaleAndCreateThumbnail(name: string) {
     const fileToConvert = path.join(videoFolder, name + ".mp4");
     const outputPath = path.join(videoFolder, "generated");
 
-    console.log(
-      "*** fileExists",
-      `${outputPath}/${name}.mp4`,
-      fs.existsSync(`${outputPath}/${name}.mp4`)
+    child_process.execSync(
+      `ffmpeg -n -i ${fileToConvert} -vf "scale=1280x720" ${outputPath}/${name}.mp4`,
+      {
+        stdio: Object.values({
+          stdin: "inherit",
+          stdout: "inherit",
+          stderr: "inherit",
+        }),
+      }
     );
-    if (!fs.existsSync(`${outputPath}/${name}.mp4`)) {
-      child_process.execSync(
-        `ffmpeg -n -i ${fileToConvert} -vf "scale=1280x720" ${outputPath}/${name}.mp4`,
-        {
-          stdio: Object.values({
-            stdin: "inherit",
-            stdout: "inherit",
-            stderr: "inherit",
-          }),
-        }
-      );
-    }
 
-    if (!fs.existsSync(`${outputPath}/${name}_thumbnail.png`)) {
-      child_process.execSync(
-        `ffmpeg -n -i ${fileToConvert} -frames:v 1 ${outputPath}/${name}_thumbnail.png`,
-        {
-          stdio: Object.values({
-            stdin: "inherit",
-            stdout: "inherit",
-            stderr: "inherit",
-          }),
-        }
-      );
-    }
+    child_process.execSync(
+      `ffmpeg -n -i ${fileToConvert} -frames:v 1 ${outputPath}/${name}_thumbnail.png`,
+      {
+        stdio: Object.values({
+          stdin: "inherit",
+          stdout: "inherit",
+          stderr: "inherit",
+        }),
+      }
+    );
   } catch (exception: any) {
     console.warn(exception.message);
   }
 }
 
-fs.readdir(videoFolder, function (err, files) {
+fs.readdir(videoFolder, (err, files) => {
   //handling error
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
   //listing all files using forEach
-  files.forEach(function (file) {
+  files.forEach((file) => {
     // Do whatever you want to do with the file
     if (file.endsWith(".mp4")) {
       scaleAndCreateThumbnail(file.replace(".mp4", ""));
--- start.sh
+++ start.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
+
+#echo $1
+
+#if [ $1 == 'full' ] 
+#then
+echo "################"
+echo "Download latest"
+echo "################"
+git pull
+
+echo "################"
+echo "Prepare frontend"
+echo "################"
 cd frontend
 npm install
 npm run build
 cd ..
+echo ""
+echo "################"
+echo "Prepare backend"
+echo "################"
+echo ""
 rm -rf backend/src/public/assets
 rm backend/src/public/index.html
 mkdir backend/src/public
 mv -v frontend/dist/* backend/src/public
 cd backend
 npm install
+#fi
+
+#if [$1 == 'full'] || [$1 == 'video']
+#then
+echo ""
+echo "################"
+echo "Optimize videos and create posters"
+echo "################"
+echo ""
 ts-node scripts/generate-video-and-poster.ts
+#fi
+
+
+echo "################"
+echo "Start server"
+echo "################"
 npm run start
