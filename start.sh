d7a5efba9759bf6778fea0a36913966ed7a7c95d
Johan Oliwsson <johan@oliwsson.com>
2023-11-23 20:37:34 +0100

    Add dependency installs in startup

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd frontend
+npm install
 npm run build
 cd ..
 rm -rf backend/src/public/assets
@@ -7,5 +8,6 @@ rm backend/src/public/index.html
 mkdir backend/src/public
 mv -v frontend/dist/* backend/src/public
 cd backend
+npm install
 ts-node scripts/generate-video-and-poster.ts
 npm run start
