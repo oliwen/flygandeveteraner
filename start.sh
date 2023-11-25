2a07eb5a03c7c40d1fe55df0ce4d06a1c79b8631
Johan Oliwsson <johan@oliwsson.com>
2023-11-25 20:16:44 +0100

    Fix minor issues

--- start.sh
+++ start.sh
@@ -13,20 +13,10 @@ echo "################"
 echo "Prepare backend"
 echo "################"
 
-mv -v frontend/dist/assets backend/src/public
 cd backend
 npm install
 else
 cd backend
-fi
-
-if [ "$arg" = "full" ] || [ "$arg" = "videos" ]; then
-echo ""
-echo "################"
-echo "Optimize videos and create posters"
-echo "################"
-echo ""
-ts-node scripts/generate-video-and-poster.ts
 fi
 
 
